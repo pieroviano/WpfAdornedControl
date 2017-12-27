85f087db340644adcc0d0c0566e0fe503be21063
Piero Viano <piero.viano@gmail.com>
2017-12-27 09:01:01 +0200

    Added extension

---
+++ WpfAdornedControl/AdornedControlWithLoadingWait.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using WpfAdornedControl.WpfControls;
+
+namespace WpfAdornedControl
+{
+    public sealed class AdornedControlWithLoadingWait : AdornedControl
+    {
+        private AdornedControlWithLoadingWait(FrameworkElement control)
+        {
+            var loadingWait = new LoadingWait();
+            AdornerContent = loadingWait;
+            ExternalGrid = new Grid();
+            ExternalGrid.Children.Add(control);
+            AddChild(ExternalGrid);
+        }
+
+        public Grid ExternalGrid { get; set; }
+
+        public static AdornedControlWithLoadingWait AdornControl(IAddChild parentElement, FrameworkElement control)
+        {
+            var adornedControlWithLoadingWait = new AdornedControlWithLoadingWait(control);
+            parentElement.AddChild(adornedControlWithLoadingWait);
+            return adornedControlWithLoadingWait;
+        }
+    }
+}
