14a73531c91e38115c6f6a587eed1119238f5767
Piero Viano <piero.viano@gmail.com>
2017-12-27 07:13:29 +0200

    Cleaned code

--- WpfAdornedControl/WpfControls/Extensions/LoadingAdornerExtension.cs
+++ WpfAdornedControl/WpfControls/Extensions/LoadingAdornerExtension.cs
@@ -2,13 +2,12 @@
 
 namespace WpfAdornedControl.WpfControls.Extensions
 {
-    public static class LoadingAdornerxtension
+    public static class LoadingAdornerExtension
     {
         public static void StartStopWait(this WpfAdornedControl.AdornedControl loadingAdorner, UIElement uiElement)
         {
             loadingAdorner.IsAdornerVisible = !loadingAdorner.IsAdornerVisible;
 
-            //WebBrowser.IsEnabled = !WebBrowser.IsEnabled;
             uiElement.Visibility = uiElement.Visibility == Visibility.Visible // condition
                 ? Visibility.Collapsed // if-case
                 : Visibility.Visible; // else-case
